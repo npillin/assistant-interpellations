2ad75904087c3eaf00643c06ab4f62286cc69118
npillin <166485905+npillin@users.noreply.github.com>
2024-05-29 11:14:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def check_password():
 
     # Show input for password.
     st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
+        "Mot de passe", type="password", on_change=password_entered, key="password"
     )
     if "password_correct" in st.session_state:
         st.error("😕 Mot de passe incorrect")
